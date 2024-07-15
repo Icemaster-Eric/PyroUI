ca0453d954458845373714a64906480fe614d3b1
Eric Li <icemastereric@gmail.com>
2024-07-15 14:25:46 -0600

    more work on buttons, initial input design

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ customElements.define("pyro-button", class extends HTMLButtonElement {
         this.style.display = "block";
     }
 }, {extends: "button"});
+
+/* need to rework the code below */
+customElements.define("pyro-input", class extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+        const input = document.createElement("input");
+        this.shadowRoot.appendChild(input);
+    }
+
+    connectedCallback() {
+        const input = this.shadowRoot.querySelector("input");
+        // Copy attributes from pyro-input to the internal input
+        for (let attr of this.attributes) {
+            input.setAttribute(attr.name, attr.value);
+        }
+        // Reflect attributes
+        new MutationObserver(mutations => {
+            for (const mutation of mutations) {
+                if (mutation.type === "attributes") {
+                    input.setAttribute(mutation.attributeName, this.getAttribute(mutation.attributeName));
+                }
+            }
+        }).observe(this, {attributes: true});
+    }
+});
